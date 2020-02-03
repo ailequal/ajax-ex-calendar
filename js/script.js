b63f1ee3234950964129194b259b7662ca6cd600
ailequal <37016865+ailequal@users.noreply.github.com>
2020-02-03 19:36:09 +0100

    addRed(month) is working

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@ $(document).ready(function() {
 
 
   // moment.js
+  // january
   var january = moment("2018-01-01", "YYYY-MM-DD").locale('it');
-  console.log(january);
+  // scan all the month
   for (var i = 0; i < 31; i++) {
     var day = january.format("dddd DD MMMM");
     var dayElement = january.format("YYYY-MM-DD");
@@ -18,13 +19,18 @@ $(document).ready(function() {
       'data-element' : dayElement
     };
     var html = template(context);
+    // add the day to the html
     $('.calendar ul').append(html);
     january.add(1, 'day');
   };
 
 
+  // test
+  var month = addRed(0);
+
+
   // function
-  // add red to holidays
+  // add red to holidays for the selected month
   function addRed(month) {
     $.ajax({
       url: "https://flynn.boolean.careers/exercises/api/holidays",
@@ -34,16 +40,26 @@ $(document).ready(function() {
         'month' : month
       },
       success: function(data, state) {
-        console.log(data.response);
+        // array with holiday objects
+        var array = data.response;
+        // scan the array
+        for (var i = 0; i < array.length; i++) {
+          // scan every day inside the html
+          $('.calendar ul li').each(function() {
+            var dataElement = $(this).attr('data-element');
+            var arrayElement = array[i].date;
+            // if data-element match add red
+            if (dataElement == arrayElement) {
+              $(this).addClass('red');
+            }
+          });
+        }
       },
       error: function(request, state, error) {
         console.log(error);
       }
     });
   }
-
-  // test
-  var december = addRed(11);
 
 });
 
