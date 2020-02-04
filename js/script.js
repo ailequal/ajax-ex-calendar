ce566f7c02c55b1f8611fde6a146c497c6908397
ailequal <37016865+ailequal@users.noreply.github.com>
2020-02-04 16:47:33 +0100

    Added holiday next to the day

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
     'year' : year,
     'month' : month,
     'day' : day
-  });
+  }).locale('it');
 
   // cycle through all days inside the month
   for (var i = 1; i <= 31; i++) {
@@ -36,6 +36,8 @@ $(document).ready(function() {
     $('.calendar ul').append(html);
   }
 
+  addHolidays(month);
+
   // function
   // show next month
   function nextMonth() {
@@ -45,6 +47,39 @@ $(document).ready(function() {
   // show previous month
   function previousMonth() {
     // code
+  }
+
+  // add holidays for the selected month and make them red
+  function addHolidays(month) {
+    $.ajax({
+      url: "https://flynn.boolean.careers/exercises/api/holidays",
+      method: "GET",
+      data: {
+        'year' : year,
+        'month' : month
+      },
+      success: function(data, state) {
+        // array with holiday objects
+        var array = data.response;
+        // scan the array
+        for (var i = 0; i < array.length; i++) {
+          // scan every day inside the html
+          $('.calendar ul li').each(function() {
+            var dataElement = $(this).attr('data-element');
+            var arrayElement = array[i].date;
+            var arrayName = array[i].name;
+            // if data-element matches add it and make it red
+            if (dataElement == arrayElement) {
+              $(this).addClass('red');
+              $(this).append(' - ' + arrayName);
+            }
+          });
+        }
+      },
+      error: function(request, state, error) {
+        console.log(error);
+      }
+    });
   }
 
   // add a zero to a number that is < 10
